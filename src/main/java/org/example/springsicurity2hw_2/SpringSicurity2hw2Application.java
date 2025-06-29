46ad6701aad2e7d5b79d54648f9d5046b1a25b1b
Sergey Rogozhkin <r8damik@gmail.com>
2025-06-29 21:05:47 +0400

    sec com

---
+++ src/main/java/org/example/springsicurity2hw_2/SpringSicurity2hw2Application.java
@@ -0,0 +1,13 @@
+package org.example.springsicurity2hw_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSicurity2hw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSicurity2hw2Application.class, args);
+    }
+
+}
